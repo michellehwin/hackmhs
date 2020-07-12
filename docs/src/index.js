53b79cdec17e25e65a87d5182dd72b2c70713f9a
michellehwin <michellehwin@gmail.com>
2020-07-12 00:51:32 -0500

    🚀create website

---
+++ docs/src/index.js
@@ -0,0 +1,12 @@
+import anime from './anime.js';
+import './styles/main.scss'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// anime({
+//     targets: 'div',
+//     translateX: 250,
+//     rotate: '1turn',
+//     backgroundColor: '#FFF',
+//     duration: 800
+// });
---
+++ docs/webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+    output: {
+        path: __dirname,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: 'html-loader',
+                    options: {
+                        minimize: true
+                    },
+                }]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({ template: "./src/index.html", filename: "./index.html" }),
+        new MiniCssExtractPlugin({ filename: "[name].css", chunkFilename: "[id].css" }),
+    ]
+}
